e5c7cc8485f725acabe9d6b1a64ef566c9d45fd7
陈刑 <amaliegay@outlook.com>
2024-05-15 11:46:33 +0800

    v2.0.0

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const XLSX = require("xlsx");
 const fs = require("fs");
+const richConsle = require("rich-console");
 
 let excel_file = "";
 let address_column = "";
@@ -19,10 +20,9 @@ neighborhood_col_name = "所属居委";
 neighborhood_column = [];
 
 const workbook = XLSX.readFile(excel_file);
-const worksheet = workbook.Sheets[workbook.SheetNames[0]];
-const addresses = XLSX.utils
-  .sheet_to_json(worksheet)
-  .map((row) => row[address_column]);
+const sheet_name = workbook.SheetNames[0];
+const worksheet = workbook.Sheets[sheet_name];
+const worksheet_json = XLSX.utils.sheet_to_json(worksheet);
 
 let rawdata = fs.readFileSync("streets.json");
 let streets = JSON.parse(rawdata);
@@ -48,25 +48,55 @@ const find_neighborhood = (address, streets) => {
           break;
         } else {
           const address_number = get_address_number(street, address);
-          if (neighborhood["oddity"] == address_number % 2)
+          if (neighborhood["oddity"] == address_number % 2) {
             if (
-              neighborhood["start"] <=
-              address_number <=
-              neighborhood["end"]
+              neighborhood["start"] <= address_number &&
+              address_number <= neighborhood["end"]
             ) {
               neighborhood_name = neighborhood["name"];
               break;
             }
+          }
         }
       }
       break;
     }
   }
+  if (neighborhood_name) {
+    richConsle.log(
+      `<blue>${neighborhood_name}</blue> <green>${address}</green>`
+    );
+  } else {
+    richConsle.log(`<red>未知</red> <green>${address}</green>`);
+  }
   return neighborhood_name;
 };
 
-for (const address of addresses) {
-  console.log(address);
-  const neighborhood_name = find_neighborhood(address, streets);
-  console.log(neighborhood_name);
+for (const row of worksheet_json) {
+  const address = row[address_column];
+  row[neighborhood_col_name] = find_neighborhood(address, streets);
 }
+
+worksheet_json.sort((row1, row2) => {
+  const address1 = row1[address_column];
+  const address2 = row2[address_column];
+  if (address1 < address2) {
+    return 1;
+  } else if (address2 > address1) {
+    return -1;
+  }
+  return 0;
+});
+worksheet_json.sort((row1, row2) => {
+  const neighborhood1 = row1[neighborhood_col_name];
+  const neighborhood2 = row2[neighborhood_col_name];
+  if (neighborhood1 < neighborhood2) {
+    return 1;
+  } else if (neighborhood2 > neighborhood1) {
+    return -1;
+  }
+  return 0;
+});
+
+workbook.Sheets[sheet_name] = XLSX.utils.json_to_sheet(worksheet_json);
+XLSX.writeFile(workbook, excel_file);
