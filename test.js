f2e465a99fdc67e358049ca39319906d523339b5
陈刑 <dev.chenxing@gmail.com>
2026-07-01 17:35:25 +0800

    feat: add input address resolver script for neighborhood lookup

---
+++ test.js
@@ -0,0 +1,16 @@
+const { 匹配所属社区 } = require("./resolver.js");
+
+const 输入地址 = process.argv.slice(2).join(" ");
+
+if (!输入地址) {
+  console.error("❌ 请输入要查询的地址，例如：node test.js 荔德路318号");
+  process.exit(1);
+}
+
+const 社区名称 = 匹配所属社区(输入地址);
+
+if (社区名称) {
+  console.log(社区名称);
+} else {
+  console.log(`❌ 未找到地址 "${输入地址}" 的所属社区`);
+}
