411df2e174a1eb146447e8b989f78df507b6c99d
陈刑 <dev.chenxing@gmail.com>
2026-07-13 16:28:47 +0800

    feat: add NeighborhoodLocator svelte component

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,10 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import svelte from '@astrojs/svelte';
 import UnoCSS from "unocss/astro";
+
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://tdjw.chenxing.dev",
+  base: "/",
   integrations: [
+    svelte(),
     UnoCSS({
       injectReset: true,
     }),
---
+++ apps/web/src/components/NeighborhoodLocator.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { 匹配所属社区 } from "@core/resolver";
+
+  let input = $state("");
+
+  const output = $derived(input ? 匹配所属社区(input) || "？？？" : "");
+</script>
+
+<!-- 移动端上下，桌面端左右 -->
+<div class="flex flex-col md:flex-row items-center gap-8">
+  <div class="md:w-80 shrink-0">
+    <input
+      type="text"
+      id="addrInput"
+      placeholder="输入地址，例如：同德街鹅掌坦"
+      class="w-full border px-3 py-2 rounded"
+      bind:value={input}
+    />
+  </div>
+  <div class="grow">
+    <div class="text-xl">{output}</div>
+  </div>
+</div>
